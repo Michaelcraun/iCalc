cfe2c3049535a6d581e5c51796059b2f90852336
Michael Craun <michaelcraun@Michaels-MacBook-Air.local>
2017-04-26 12:59:44 -0400

    Initial Commit

---
+++ iCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCalc
+//
+//  Created by Michael Craun on 4/26/17.
+//  Copyright © 2017 Michael Craun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
